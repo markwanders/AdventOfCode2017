4bce77bc63555af8632c96e9c78f9d6e44c846d3
Mark Wanders <mndmwanders@gmail.com>
2021-08-11 16:31:15 +0200

    Solved day 6 part 2

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,23 +1,31 @@
 fun main() {
     val banks = input.split("\t").mapIndexed { index, s -> index to s.toInt() }.toMap().toMutableMap()
-    val configurations = mutableListOf(banks.values.joinToString("-"))
-    var proceed = true
-    while (proceed) {
+    val configurations = mutableListOf(banks.toString())
+    var loop = ""
+    var loopSize = 1
+    while (true) {
         val max = banks.maxByOrNull { it.value }!!
         val distribute = max.value
         banks[max.key] = 0
         for (i in 1..distribute) {
             val bankNr = (i + max.key) % banks.size
-            banks[bankNr] = banks[bankNr]!!.plus(1)
+            banks[bankNr] = banks[bankNr]!! + 1
         }
-        val configuration =  banks.values.joinToString("-")
-        if(configuration in configurations) {
-            proceed = false
+        val configuration = banks.toString()
+        if(configuration in configurations && loop.isEmpty()) {
+            println("Answer part 1: ${configurations.size}")
+            loop = banks.toString()
+        } else if (banks.toString() == loop) {
+            println("Answer part 2: $loopSize")
+            return
         } else {
-            configurations.add(configuration)
+            if (loop.isNotEmpty()) {
+                loopSize += 1
+            } else {
+                configurations.add(configuration)
+            }
         }
     }
-    println(configurations.size)
 }
 
 private const val input = "4\t10\t4\t1\t8\t4\t9\t14\t5\t1\t14\t15\t0\t15\t3\t5"
