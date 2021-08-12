f73e19841d2207b2a0b0efd6da8abd14ff26f5cb
Mark Wanders <mndmwanders@gmail.com>
2021-08-12 21:23:51 +0200

    Solved day 7 part 2

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -1,17 +1,41 @@
 import java.io.File
 
+var contains = mapOf<String, List<String>>()
+var weightPerProgram = mapOf<String, Int>()
+
 fun main() {
-    val towers = input.map {
-        Regex("[^A-Za-z,)]").replace(it, "")
+    val towers = mutableListOf<List<String>>()
+    val weights = mutableListOf<Int>()
+    input.forEach {
+        towers.add(Regex("[^A-Za-z,)]").replace(it, "")
             .replace(")", ",")
             .split(",")
-            .filter { it.isNotBlank() }
+            .filter { it.isNotBlank() })
+        weights.add(Regex("[^0-9]").replace(it, "").toInt())
     }
-    val contains = towers.associate { tower ->
+    contains = towers.associate { tower ->
         tower[0] to tower.subList(1, tower.size)
     }
     val contained = contains.values.toList().flatten()
-    println(contains.keys.filter { it !in contained })
+    val bottom = contains.keys.first { it !in contained }
+    println("Answer part 1: $bottom")
+    weightPerProgram = towers.map { it.first() }.zip(weights).toMap()
+
+    totalWeight(bottom)
 }
 
+fun totalWeight(program: String): Int {
+    val subTowers = contains[program]!!.map { totalWeight(it) }
+    val weightOccurrences = subTowers.groupingBy { it }.eachCount()
+    if (weightOccurrences.size > 1) {
+            val indexOdd = subTowers.indexOf(weightOccurrences.keys.first { weightOccurrences[it] == 1 })
+            val wrongWeight = contains[program]!!.map { weightPerProgram[it] }[indexOdd]
+            println("Answer part 2: ${wrongWeight!! + (weightOccurrences.keys.first() - weightOccurrences.keys.last())}")
+            return weightPerProgram[program]!! + subTowers.sum() + (weightOccurrences.keys.first() - weightOccurrences.keys.last())
+    }
+    return weightPerProgram[program]!! + subTowers.sum()
+}
+
+
 private val input = File("src/main/resources/day07.txt").readLines()
+private val test = File("src/main/resources/day07_test.txt").readLines()
